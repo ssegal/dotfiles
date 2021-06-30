da0eefd29975a0285faafaa79b0417e09b060e29
Stephen Segal <stephen.segal@getcruise.com>
2021-06-30 15:16:19 -0700

    add tmux config

--- install.zsh
+++ install.zsh
@@ -8,8 +8,9 @@ dotfiles=$(realpath --relative-to="$HOME" "$dotfiles_absolute")
 mkdir -p "$HOME/.local/share"
 ln -Tfs $(realpath --relative-to="$HOME/.local/share" "$dotfiles_absolute/antigen") $HOME/.local/share/zsh-antigen
 
-rm -rf $HOME/.antigenrc $HOME/.zprofile $HOME/.zshrc $HOME/.emacs.d
+rm -rf $HOME/.antigenrc $HOME/.zprofile $HOME/.zshrc $HOME/.emacs.d $HOME/.tmux.conf
 ln -fs "$dotfiles/antigenrc" "$HOME/.antigenrc"
 ln -fs "$dotfiles/zprofile" "$HOME/.zprofile"
 ln -fs "$dotfiles/zshrc" "$HOME/.zshrc"
 ln -Tfs "$dotfiles/emacs.d" "$HOME/.emacs.d"
+ln -fs "$dotfiles/tmux.conf" "$HOME/.tmux.conf"
