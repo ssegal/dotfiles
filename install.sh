5fc145a14ef1c91bfde78e871b81f3c3e942d72a
Stephen Segal <ssegal127@gmail.com>
2026-05-25 11:13:48 -0400

    Support Linux without homebrew
    Once again we support systems without Homebrew, but only Linux.  This is
    to allow use on shared systems where installing homebrew would not be
    appropriate.

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 #
-# This script is intended to operate on any POSIX-compliant shell.
+# Assume that Linux systems have GNU coreutils or compatible.
 
 set -eu
+
+abort() {
+    echo "$*" >&2
+    exit 1
+}
+
+[ -n "${BASH_VERSION:-}" ] || abort "Must be run with bash"
 
 command_exists() {
     # Since we've turned on errexit, test result with || to avoid triggering it.
@@ -10,12 +17,43 @@ command_exists() {
     return 0
 }
 
+get_latest_release_tag() {
+    curl -sLS https://api.github.com/repos/$1/releases/latest | \
+        grep "tag_name" | \
+        cut -d ':' -f "2,3" | \
+        tr -d '", :'
+}
+
 cleanup() {
-    [ -n "${TMPDIR}" ] && rm -rf "${TMPDIR}"
+    [ -n "${TEMPDIR}" ] && rm -rf "${TEMPDIR}"
 }
 trap cleanup EXIT
 
-TMPDIR=$(mktemp -d)
+TEMPDIR=$(mktemp -d)
+
+command_exists curl || abort "$0: curl missing"
+command_exists xz || abort "$0: xz missing"
+
+if command_exists brew; then
+    echo "*** Existing homebrew found!"
+    eval "$(brew shellenv)"
+elif [ "${2:-}" = "--brew" ]; then
+    # install homebrew
+    echo "*** Installing homebrew"
+    export NONINTERACTIVE=1
+    ${BASH} -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    if [ -x "/usr/local/bin/brew" ]; then
+        eval "$(/usr/local/bin/brew shellenv)"
+    elif [ -x "/opt/homebrew/bin/brew" ]; then
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+    elif [ -x "/home/linuxbrew/.linuxbrew/bin/brew" ]; then
+        eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    fi
+elif [ "$(uname -s)" = "Darwin" ]; then
+    abort "$0: MacOS requires homebrew.  Use \"--brew\"."
+else
+    echo "*** No homebrew installation"
+fi
 
 echo "*** Removing old links"
 rm -rf \
@@ -27,49 +65,122 @@ rm -rf \
 
 HOME_BIN_DIR="$HOME/.local/bin"
 mkdir -p "$HOME_BIN_DIR"
+HOME_MAN_DIR="$HOME/.local/share/man"
+mkdir -p "$HOME_MAN_DIR"
 
-if command_exists brew; then
-    echo "*** Existing homebrew found!"
-    BREW="brew"
-else
-    # install homebrew
-    echo "*** Installing homebrew"
-    export NONINTERACTIVE=1
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    if [ -x "/usr/local/bin/brew" ]; then
-        BREW="/usr/local/bin/brew"
-    elif [ -x "/opt/homebrew/bin/brew" ]; then
-        BREW="/opt/homebrew/bin/brew"
-    elif [ -x "/home/linuxbrew/.linuxbrew/bin/brew" ]; then
-        BREW="/home/linuxbrew/.linuxbrew/bin/brew"
-    fi
-fi
-
-HOMEBREW_PREFIX="$(${BREW} --prefix)"
 REALPATH="realpath"
 GREP="grep"
 LN="ln"
-BASH="bash"
 [ -z "${LANG:-}" ] && export LANG=en_US.UTF-8
 [ -z "${USER:-}" ] && export USER="$(id -un)"
 
-export PATH="$HOMEBREW_PREFIX/bin":"$PATH"
-
-echo "*** Installing extra tools via Homebrew"
-brew install -q rg eza bat bat-extras fzf lazygit fd starship
-if [ "$(uname -s)" = "Darwin" ]; then
-    echo "*** MacOS-specific install"
-    brew install -q coreutils grep bash bash-completion@2 findutils gnu-sed gnu-tar gawk git
-    REALPATH="grealpath"
-    GREP="ggrep"
-    LN="gln"
-    BASH="$HOMEBREW_PREFIX/bin/bash"
-    if ! ${GREP} -Fq "${BASH} /etc/shells"; then
-        echo "${BASH}" | sudo tee -a /etc/shells > /dev/null
+if [ -n "${HOMEBREW_PREFIX:-}" ]; then
+    echo "*** Installing extra tools via Homebrew"
+    brew install -q rg eza bat bat-extras fzf lazygit fd starship
+    if [ "$(uname -s)" = "Darwin" ]; then
+        echo "*** MacOS-specific install"
+        brew install -q coreutils grep bash bash-completion@2 findutils gnu-sed gnu-tar gawk git
+        REALPATH="grealpath"
+        GREP="ggrep"
+        LN="gln"
+        LOGIN_BASH="$HOMEBREW_PREFIX/bin/bash"
+        if ! ${GREP} -Fq "${BASH} /etc/shells"; then
+            echo "${LOGIN_BASH}" | sudo tee -a /etc/shells > /dev/null
+        fi
+        sudo chsh "$(whoami)" -s "$BASH"
     fi
-    sudo chsh "$(whoami)" -s "$BASH"
+else
+    echo "*** Installing extra tools"
+
+    # use statically-linked musl variants when available.
+    case "$(uname -sm)" in
+        "Linux aarch64")
+            ripgrep_triple=aarch64-unknown-linux-gnu
+            eza_triple=aarch64-unknown-linux-gnu
+            bat_triple=aarch64-unknown-linux-musl
+            fd_triple=aarch64-unknown-linux-musl
+            fzf_triple=linux_arm64
+            ;;
+        "Linux x86_64")
+            ripgrep_triple=x86_64-unknown-linux-musl
+            eza_triple=x86_64-unknown-linux-musl
+            bat_triple=x86_64-unknown-linux-musl
+            fd_triple=x86_64-unknown-linux-musl
+            fzf_triple=linux_amd64
+            ;;
+        *)
+            abort "$0: Unsupported machine type"
+            ;;
+    esac
+
+    if ! command_exists starship; then
+        echo "*** Installing starship"
+        curl -sS https://starship.rs/install.sh | sh -s -- -f -b "${HOME_BIN_DIR}"
+    fi
+    if ! command_exists rg && [ -n "${ripgrep_triple:-}" ]; then
+        echo "*** Downloading rg"
+        ripgrep_tag=$(get_latest_release_tag BurntSushi/ripgrep)
+        curl -sLS "https://github.com/BurntSushi/ripgrep/releases/download/${ripgrep_tag}/ripgrep-${ripgrep_tag}-${ripgrep_triple}.tar.gz" | \
+            tar xz -C "$TEMPDIR"
+        cp -f "$TEMPDIR/ripgrep-${ripgrep_tag}-${ripgrep_triple}/rg" "$HOME_BIN_DIR"
+        chmod +x "$HOME_BIN_DIR/rg"
+        mkdir -p "${HOME_MAN_DIR}/man1"
+        cp -f "$TEMPDIR/ripgrep-${ripgrep_tag}-${ripgrep_triple}/doc/rg.1" "$HOME_MAN_DIR/man1"
+        mkdir -p "$HOME/.local/share/bash-completions/completions"
+        cp -f "$TEMPDIR/ripgrep-${ripgrep_tag}-${ripgrep_triple}/complete/rg.bash" "$HOME/.local/share/bash-completions/completions/rg"
+    fi
+    if ! command_exists eza && [ -n "${eza_triple:-}" ]; then
+        echo "*** Downloading eza"
+        eza_tag=$(get_latest_release_tag eza-community/eza)
+        eza_version=${eza_tag#?}
+        curl -sLS "https://github.com/eza-community/eza/releases/download/${eza_tag}/eza_${eza_triple}.tar.gz" | \
+            tar xz -C "$HOME_BIN_DIR" ./eza
+        chmod +x "$HOME_BIN_DIR/eza"
+        curl -sLS "https://github.com/eza-community/eza/releases/download/${eza_tag}/completions-${eza_version}.tar.gz" | \
+            tar xz -C "$TEMPDIR"
+        mkdir -p "$HOME/.local/share/bash-completions/completions"
+        cp -f "$TEMPDIR/target/completions-${eza_version}/eza" "$HOME/.local/share/bash-completions/completions"
+        curl -sLS "https://github.com/eza-community/eza/releases/download/${eza_tag}/man-${eza_version}.tar.gz" | \
+            tar xz -C "$TEMPDIR"
+        mkdir -p "$HOME_MAN_DIR/man1" "$HOME_MAN_DIR/man5"
+        cp -f "$TEMPDIR/target/man-${eza_version}/eza.1" "$HOME_MAN_DIR/man1"
+        cp -f "$TEMPDIR/target/man-${eza_version}/eza_colors.5" "$HOME_MAN_DIR/man5"
+        cp -f "$TEMPDIR/target/man-${eza_version}/eza_colors-explanation.5" "$HOME_MAN_DIR/man5"
+    fi
+    if ! command_exists bat && [ -n "${bat_triple:-}" ]; then
+        echo "*** Downloading bat"
+        bat_tag=$(get_latest_release_tag sharkdp/bat)
+        curl -sLS "https://github.com/sharkdp/bat/releases/download/${bat_tag}/bat-${bat_tag}-${bat_triple}.tar.gz" | \
+            tar xz -C "$TEMPDIR"
+        cp -f "$TEMPDIR/bat-${bat_tag}-${bat_triple}/bat" "$HOME_BIN_DIR"
+        chmod +x "$HOME_BIN_DIR/bat"
+        mkdir -p "${HOME_MAN_DIR}/man1"
+        cp -f "$TEMPDIR/bat-${bat_tag}-${bat_triple}/bat.1" "${HOME_MAN_DIR}/man1/bat.1"
+        mkdir -p "$HOME/.local/share/bash-completions/completions"
+        cp -f "$TEMPDIR/bat-${bat_tag}-${bat_triple}/autocomplete/bat.bash" "$HOME/.local/share/bash-completions/completions/bat"
+    fi
+    if ! command_exists fd && [ -n "${fd_triple:-}" ]; then
+        echo "*** Downloading fd"
+        fd_tag=$(get_latest_release_tag sharkdp/fd)
+        curl -sLS "https://github.com/sharkdp/fd/releases/download/${fd_tag}/fd-${fd_tag}-${fd_triple}.tar.gz" | \
+            tar xz -C "$TEMPDIR"
+        cp -f "$TEMPDIR/fd-${fd_tag}-${fd_triple}/fd" "$HOME_BIN_DIR"
+        mkdir -p "${HOME_MAN_DIR}/man1"
+        cp -f "$TEMPDIR/fd-${fd_tag}-${fd_triple}/fd.1" "${HOME_MAN_DIR}/man1/fd.1"
+        chmod +x "$HOME_BIN_DIR/fd"
+        mkdir -p "$HOME/.local/share/bash-completions/completions"
+        cp -f "$TEMPDIR/fd-${fd_tag}-${fd_triple}/autocomplete/fd.bash" "$HOME/.local/share/bash-completions/completions/fd"
+    fi
+    if ! command_exists fzf && [ -n "${fzf_triple:-}" ]; then
+        echo "*** Downloading fzf"
+        fzf_tag=$(get_latest_release_tag junegunn/fzf)
+        fzf_version=${fzf_tag#?}
+        echo "downloading https://github.com/junegunn/fzf/releases/download/${fzf_tag}/fzf-${fzf_version}-${fzf_triple}.tar.gz"
+        curl -sLS "https://github.com/junegunn/fzf/releases/download/${fzf_tag}/fzf-${fzf_version}-${fzf_triple}.tar.gz" | \
+            tar xz -C "$HOME_BIN_DIR" fzf
+        chmod +x "$HOME_BIN_DIR/fzf"
+    fi
 fi
-command_exists xz || brew install xz
 
 DOTFILES=$(dirname "$(${REALPATH} "$0")")
 
@@ -94,12 +205,8 @@ elif [ ! -e "$HOME/.ssh/authorized_keys" ]; then
 fi
 
 echo "*** Installing ble.sh"
-curl -L https://github.com/akinomyoga/ble.sh/releases/download/nightly/ble-nightly.tar.xz | tar xJf - -C "$TMPDIR"
-$BASH "$TMPDIR"/ble-nightly/ble.sh --install ~/.local/share
-
-#echo "*** Installing bash-it"
-#git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
-#~/.bash_it/install.sh --silent --no-modify-config
+curl -sLS https://github.com/akinomyoga/ble.sh/releases/download/nightly/ble-nightly.tar.xz | tar xJf - -C "$TEMPDIR"
+$BASH "$TEMPDIR"/ble-nightly/ble.sh --install ~/.local/share
 
 echo "*** Wiring up bash config scripts"
 DOTFILES_REL=$(${REALPATH} --relative-to="$HOME" "$DOTFILES")
