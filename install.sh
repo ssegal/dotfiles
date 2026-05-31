13fd0f22275af476cd0a997d65941927215e89e8
Stephen <ssegal127@gmail.com>
2026-05-31 19:02:12 -0400

    configure nano

--- install.sh
+++ install.sh
@@ -201,9 +201,7 @@ echo "*** Creating links"
 $LN -rfs "$DOTFILES/emacs.d" "$HOME/.emacs.d"
 $LN -rfs "$DOTFILES/tmux.conf" "$HOME/.tmux.conf"
 mkdir -p "$HOME/.config"
-$LN -rfs "$DOTFILES/config/starship.toml" "$HOME/.config/starship.toml"
-$LN -rfs --target-directory="$HOME/.config" "$DOTFILES/config/bat"
-
+$LN -rfs --target-directory="$HOME/.config" $DOTFILES/config/*
 $LN -rfs "$DOTFILES/bin/edit" "$HOME_BIN_DIR/edit"
 
 echo "*** Installing SSH keys"
